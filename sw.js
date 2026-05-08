ce2b38d95524ef7a23163ab1c630dc33f0078f7e
Keegan Whitacre <keeganwhitacre@gmail.com>
2026-05-08 15:27:01 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
       to show persistent notifications on mobile.
 */
 // Bump this version (e.g., from v2 to v3) so the browser updates the cache!
-const CACHE = 'lift-v10'; 
+const CACHE = 'lift-v11'; 
 
 // Add exercises.json, plus your manifest and icon so it works 100% offline
 const SHELL = [
